f2d3597e30883440370eb13682f34d086605cfe9
Geert Berkers <geertberkers@hotmail.com>
2019-10-02 13:18:01 +0200

    Moved files in ClientRequest

--- Sources/KituraNet/ClientRequest.swift
+++ Sources/KituraNet/ClientRequest.swift
@@ -1027,3 +1027,179 @@ private struct OneTimeInitializations {
     }
 }
 
+
+let bash: CommandExecuting = Bash()
+
+protocol CommandExecuting {
+    func execute(commandName: String) -> String?
+    func execute(commandName: String, arguments: [String]) -> String?
+}
+
+final class Bash: CommandExecuting {
+    
+    // MARK: - CommandExecuting
+    
+    func execute(commandName: String) -> String? {
+        return execute(commandName: commandName, arguments: [])
+    }
+    
+    func execute(commandName: String, arguments: [String]) -> String? {
+        Log.debug("Command:\n\(commandName)")
+        
+        guard let result = execute(command: "/bin/bash" , arguments: ["-c", commandName]) else {
+            Log.error("Could NOT execute Bash Command:\n\(commandName)")
+            return nil
+        }
+        
+        Log.debug("Result:\n\(result)")
+        
+        return result
+    }
+    
+    // MARK: Private
+    
+    private func execute(command: String, arguments: [String] = []) -> String? {
+        
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: command)
+        process.arguments = arguments
+        
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        
+        do {
+            try process.run()
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            return String(data: data, encoding: String.Encoding.utf8)
+        } catch {
+            return nil
+        }
+    }
+}
+
+class OCSPChecker {
+    
+    init(url: String, projectPath: String) {
+        self.url = url
+        self.projectPath = projectPath
+    }
+    
+    var url: String
+    var projectPath: String
+    var ocspUri: String? = nil
+    
+    var hostName: String {
+        return String(url.split(separator: "/").first!)
+    }
+    
+    func checkOCSP() -> Bool {
+        Log.info("HostName: \(hostName)")
+        
+        guard let sslPath = getSSLCert() else {
+            Log.error("Could NOT download SSL Cert from : \(hostName)")
+            return false
+        }
+        
+        Log.info("SSL Path: \(sslPath)")
+        
+        guard let issuerPath = getSSLIssuerCert() else {
+            Log.error("Could NOT get issuer Cert")
+            return false
+        }
+        
+        Log.info("Issuer Path: \(issuerPath)")
+        
+        guard let ocspUri = getOCSPUri(sslPath: sslPath) else {
+            Log.error("No OCSP Uri")
+            return false
+        }
+        
+        Log.info("OCSP uri: \(ocspUri)")
+        
+        self.ocspUri = ocspUri
+        
+        guard let ocspStatus = getOCSPStatus(issuerPath: issuerPath, sslPath: sslPath, uri: ocspUri) else {
+            Log.error("No OCSP Status")
+            return false
+        }
+        
+        Log.info("OCSP Status: \(ocspStatus)")
+        
+        switch ocspStatus {
+        case let status where status.contains(": good") :
+            Log.info("OSCP Verification: Good!")
+            return true
+        case let status where status.contains(": revoked") :
+            Log.error("OSCP Verification: Revoked!")
+            return false
+        case let status where status.contains(": unknown") :
+            Log.error("OSCP Verification: Unknown!")
+            return false
+        default:
+            Log.error("OCSP: No Result!")
+            return false
+        }
+    }
+    
+    func getSSLCert() -> String? {
+        //        // NOTE: Mock Cert from local storage
+        //        return "\(bashPath)/\(hostName).crt"
+        let certPath = "\(bashPath)/\(hostName).pem"
+        let commando = "openssl s_client -connect \(hostName):443  -servername \(hostName) 2>&1 < /dev/null | sed -n '/-----BEGIN/,/-----END/p' > \(certPath)"
+        //        let commando = "openssl s_client -showcerts -connect \(hostName):443 -servername \(hostName) </dev/null 2>/dev/null|openssl x509 -text -outform PEM > \(certPath)"
+        
+        guard
+            let _ = executeSSLCommando(commando),
+            let _ = FileManager.default.contents(atPath: certPath)
+            else {
+                return nil
+        }
+        
+        return certPath
+    }
+    
+    func getOCSPUri(sslPath: String) -> String? {
+        let command = "openssl x509 -noout -ocsp_uri -in \(sslPath)"
+        return bash.execute(commandName: command)?.replacingOccurrences(of: "\n", with: "")
+    }
+    
+    
+    func getSSLIssuerCert() -> String? {
+        let certPath = "\(bashPath)/\(hostName).ca.crt"
+        
+        let command = "openssl s_client -connect \(hostName):443 -servername \(hostName) -showcerts 2>&1 < /dev/null | sed -n '/-----BEGIN/,/-----END/p' > \(certPath)"
+        
+        guard
+            let _ = executeSSLCommando(command),
+            let _ = FileManager.default.contents(atPath: certPath)
+            else {
+                return nil
+        }
+        
+        return certPath
+    }
+    
+    func getOCSPStatus(issuerPath: String, sslPath: String, uri: String) -> String? {
+        let caPath = "\(projectPath)/var/www/ca/ca-certificates.crt"
+        let command = "openssl ocsp -sha1 -issuer \(issuerPath) -cert \(sslPath) -url \(uri) -CAfile \(caPath) -no_nonce"
+        return bash.execute(commandName: command)
+    }
+    
+    
+    func executeSSLCommando(_ commando: String) -> String? {
+        return bash.execute(commandName: commando)
+    }
+}
+
+extension OCSPChecker {
+    
+    var bashPath : String {
+        return "\(projectPath)/var/bash"
+    }
+    
+    var chain: String {
+        return "\(projectPath)/var/www/ca/ca-certificates.crt"
+    }
+    
+    
+}
